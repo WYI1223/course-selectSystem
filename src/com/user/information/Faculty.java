f54c4e9ae695155d0e1605e41bc0ce98fa5090b6
W_YI <W_YI1223@outlook.com>
2022-11-26 17:20:29 +0800

    userinforrmation updata

--- src/com/user/information/Faculty.java
+++ src/com/user/information/Faculty.java
@@ -1,4 +1,6 @@
 package com.user.information;
+//用户：教师
+//
 
 public class Faculty{
 
---
+++ src/com/user/information/Memeber.java
@@ -0,0 +1,15 @@
+package com.user.information;
+//用户父类
+//名字，年龄，ID
+
+
+public class Memeber {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int ID;
+
+
+
+
+}
