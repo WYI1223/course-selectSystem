6255167a4f62155f5fe25d327ac6c877d211eafc
NHajime3000 <machen@kean.edu>
2022-12-09 00:05:31 +0800

    Update Test.java

--- src/com/user/information/Test.java
+++ src/com/user/information/Test.java
@@ -17,31 +17,7 @@ public class Test {
         String major = input.next();
         Students Sun = new Students(infor1,fname,lname,Greades,major);
         
-        System.out.println("choose function: \n1.check course list\n2.check shopping list");
-        int choice = input.nextInt();
-        switch(choice){
-            case 1:checkCourseList();break;
-            case 2:checkShoppingList();break;
-        }
 
     }
 
-    public static void checkCourseList(){
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("chose the function you want to do:\n1.according to level\n2.according to credits\n3.according to available seats\n4.according to date");
-
-        int choice = input.nextInt();
-
-        switch(choice){
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-        }
-    }
-
-    public static void checkShoppingList(){
-
-    }
 }
