45f3cfbb1db975c4935cd59cfc114ee992409607
NHajime3000 <machen@kean.edu>
2022-12-09 02:16:24 +0800

    浏览购物车的分支功能

--- src/com/user/information/Students.java
+++ src/com/user/information/Students.java
@@ -17,13 +17,14 @@ import com.course.information.shopping;
 public class Students extends Member implements Serializable {
 
 
-
-
     private String major;
     private int id;
     private int grade;
     private int creditsTotal;
     private int creditsAvailable;
+
+    //该学生的购物车
+    private static shopping shoppingCart;
 
     //已修课程
     private ArrayList<Course> courseList;
@@ -74,12 +75,22 @@ public class Students extends Member implements Serializable {
         return creditsAvailable;
     }
 
+    //将序号（）加入课程购物车
+    public void addShoppingCourse(Course i){
+        shoppingCart.addCourse(i);
+    }
+
+    //购物车删除序号（）课程
+    public void delShoppingCourse(Course i){
+        shoppingCart.delCourse(i);
+    }
+
     public static void main(String[] args) {
         //学生可在此程序运行的方法
         //1.添加课程（课程会筛查重复时间以及内容）
         //2.删除课程
         //打印课程表
-        
+       
         Scanner input = new Scanner(System.in);
         System.out.println("Enter you acount(Idnumber + firstname + lastname + grades + major):");
         int infor1 = input.nextInt();
@@ -88,19 +99,21 @@ public class Students extends Member implements Serializable {
         int Greades = input.nextInt();
         String major = input.next();
         
-        //选择功能1.浏览课程表2.浏览购物车
-        System.out.println("choose function: \n1.check course list\n2.check shopping list");
-        int choice = input.nextInt();
-        switch(choice){
-            case 1->checkCourseList();
-            case 2->checkShoppingList();
+        for(;;){
+            //选择功能1.浏览课程表2.浏览购物车
+            System.out.println("choose function: \n1.check course list\n2.check shopping list");
+            int choice = input.nextInt();
+            switch(choice){
+                case 1->checkCourseList();
+                case 2->checkShoppingList();
+            }
         }
     }
 
     public static void checkCourseList(){
         Scanner input = new Scanner(System.in);
 
-        //新建shopping对象
+        //新建shopping对象(此对象涵盖全课表)
         shopping shop = new shopping();
         
         //选择功能1.level排序2.学分排序3.可用座位排序4.时间排序
@@ -119,12 +132,34 @@ public class Students extends Member implements Serializable {
 
         Scanner input = new Scanner(System.in);
 
-        shopping shop = new shopping();
-
         //打印当前购物车
-        System.out.println(shop.toString());
+        System.out.println(shoppingCart.toString());
 
         //显示购物车学分
-        System.out.println(shop.getCredits());
+        System.out.println(shoppingCart.getCredits());
+
+        //选择功能1.删除（）课程2.浏览课程表3.返回首页
+        System.out.println("choose the function you want to do:\n1.remove x(serial number) course\n2.check course list\n3.return to menu");
+        int choice = input.nextInt();
+        switch(choice){
+            case 1:
+                for(;;){
+                    //当前购物车课程个数
+                    int i = shoppingCart.toString().length() + 1;
+
+                    //购物车内第x位课程
+                    System.out.println("which course you want to remove(serial number)");
+                    int x = input.nextInt();
+                    if(x < 0 || x > i){
+                        System.out.println("invalid number");
+                    }
+                    else shoppingCart.delCourse(shoppingCart.getCourseInfo(x));break;
+                }
+                break;
+
+            case 2:checkCourseList();break;
+            case 3:break;
+        }
+        return;
     }
 }
