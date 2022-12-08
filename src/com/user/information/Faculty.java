0a886353e247db588b1de37c4ddc4daf95fdf32d
NHajime3000 <machen@kean.edu>
2022-12-08 15:15:51 +0800

    Update Faculty.java

--- src/com/user/information/Faculty.java
+++ src/com/user/information/Faculty.java
@@ -7,6 +7,8 @@ package com.user.information;
 import com.course.information.Course;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Faculty extends Member implements Serializable {
 
@@ -26,7 +28,19 @@ public class Faculty extends Member implements Serializable {
         }
     }
 
-   
+    //删除课程
+   public void removeCourse(Course i){
+        boolean exist = new Faculty().contains(i);
+        if(exist == false)System.out.println("此课程不存在");
+        else courses.remove(i);
+   }
+
+   //检查课程列表
+   private boolean contains(Course i){
+        List<ArrayList<Course>> CourseList = Arrays.asList(courses);
+        boolean result = CourseList.contains(i);
+        return result;
+   }
 
 
 
