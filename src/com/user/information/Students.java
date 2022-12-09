fc0dda75074710fba7b5dcc4073894694297911c
Joe-xzj <xuzhij@kean.edu>
2022-12-09 14:07:32 +0800

    Revert
    This reverts commit 0432b40ff004a96b20358ab7b3d8b4107ac4f463.

--- src/com/user/information/Students.java
+++ src/com/user/information/Students.java
@@ -84,5 +84,81 @@ public class Students extends Member implements Serializable {
     public void delShoppingCourse(Course i){
         shoppingCart.delCourse(i);
     }
+    public static void main(String[] args) {
+        //学生可在此程序运行的方法
+        //1.添加课程（课程会筛查重复时间以及内容）
+        //2.删除课程
+        //打印课程表
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter you acount(Idnumber + firstname + lastname + grades + major):");
+        int infor1 = input.nextInt();
+        String fname = input.next();
+        String lname = input.next();
+        int Greades = input.nextInt();
+        String major = input.next();
+
+        for(;;){
+            //选择功能1.浏览课程表2.浏览购物车
+            System.out.println("choose function: \n1.check course list\n2.check shopping list");
+            int choice = input.nextInt();
+            switch(choice){
+                case 1->checkCourseList();
+                case 2->checkShoppingList();
+            }
+        }
+    }
+
+    public static void checkCourseList(){
+        Scanner input = new Scanner(System.in);
+
+        //新建shopping对象(此对象涵盖全课表)
+        shopping shop = new shopping();
+
+        //选择功能1.level排序2.学分排序3.可用座位排序4.时间排序
+        System.out.println("chose the function you want to do:\n1.according to level\n2.according to credits\n3.according to available seats\n4.according to date");
+        int choice = input.nextInt();
+
+        switch(choice){
+            case 1->shop.sortLevel();
+            case 2->shop.sortCre();
+            case 3->shop.sortSeat();
+            case 4->shop.sortDate();
+        }
+    }
+
+    public static void checkShoppingList(){
+
+        Scanner input = new Scanner(System.in);
+
+        //打印当前购物车
+        System.out.println(shoppingCart.toString());
+
+        //显示购物车学分
+        System.out.println(shoppingCart.getCredits());
+
+        //选择功能1.删除（）课程2.浏览课程表3.返回首页
+        System.out.println("choose the function you want to do:\n1.remove x(serial number) course\n2.check course list\n3.return to menu");
+        int choice = input.nextInt();
+        switch(choice){
+            case 1:
+                for(;;){
+                    //当前购物车课程个数
+                    int i = shoppingCart.toString().length() + 1;
+
+                    //购物车内第x位课程
+                    System.out.println("which course you want to remove(serial number)");
+                    int x = input.nextInt();
+                    if(x < 0 || x > i){
+                        System.out.println("invalid number");
+                    }
+                    else shoppingCart.delCourse(shoppingCart.getCourseInfo(x));break;
+                }
+                break;
+
+            case 2:checkCourseList();break;
+            case 3:break;
+        }
+        return;
+    }
 }
