cdbdbca0d655ab104376ad86f16d5e84594a4368
Gyy12138-66 <71025850+Gyy12138-66@users.noreply.github.com>
2022-12-11 18:32:04 +0800

    Update json2course.java

--- src/com/course/json2data/json2course.java
+++ src/com/course/json2data/json2course.java
@@ -1,6 +1,8 @@
 package com.course.json2data;
-import java.nio.file.Files;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import com.google.gson.Gson;
 
 public class json2course {
@@ -28,5 +30,28 @@ public class json2course {
 
     }
     // Example usage:
+
+    public static String readJsonDataFromFile(String filePath) throws IOException {
+        // 创建一个字符串缓冲区，用于存储文件中的JSON数据
+        StringBuilder jsonData = new StringBuilder();
+
+        // 创建一个文件读取流
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        // 定义一个变量，用于存储每一行读取到的数据
+        String line;
+
+        // 使用循环读取文件中的每一行数据
+        while ((line = reader.readLine()) != null) {
+            // 将读取到的数据添加到字符串缓冲区中
+            jsonData.append(line);
+        }
+
+        // 关闭文件读取流
+        reader.close();
+
+        // 将字符串缓冲区中的数据转换为字符串并返回
+        return jsonData.toString();
+    }
 
 }
